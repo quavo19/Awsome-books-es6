bc60a32adf9b58d59b9b16b1f4c8974ebc11d228
AkiteDonald <donaldakite27@gmail.com>
2022-12-05 12:50:16 +0000

    Remove functions and import them from the  modules folder

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 /* eslint-disable no-loop-func */
+import { date } from './modules/date-time.js';
+import { Navigation } from './modules/navigation.js';
 let book = [];
+date();
 
-function CreateBook(titleValue, authorValue, id) {
-  this.title = titleValue;
-  this.author = authorValue;
-  this.id = id;
+class CreateBook{
+    constructor(titleValue, authorValue, id){
+    this.title = titleValue;
+    this.author = authorValue;
+    this.id = id;
+    }
 }
 
 const pushBook = () => {
@@ -18,8 +23,8 @@ const pushBook = () => {
   }
 };
 
-const bookList = document.querySelector('.books-list');
 const createBook = () => {
+const bookList = document.querySelector('.books-list');
   bookList.innerHTML = ' ';
   book = JSON.parse(localStorage.getItem('book')) || [];
 
@@ -79,74 +84,4 @@ addBtn.addEventListener('click', () => {
 book = JSON.parse(localStorage.getItem('book')) || [];
 createBook();
 
-const homePage = () => {
-  const addNewBook = document.querySelector('.new-book');
-  if (addNewBook.classList.contains('vanish') === false) {
-    addNewBook.classList.add('vanish');
-  }
-  const contact = document.querySelector('.contact');
-  if (contact.classList.contains('vanish') === false) {
-    contact.classList.add('vanish');
-  }
-  const bookDisplay = document.querySelector('.awesome-books');
-  if (bookDisplay.classList.contains('vanish') === true) {
-    bookDisplay.classList.remove('vanish');
-  }
-};
-
-homePage();
-
-const contactArea = () => {
-  const addNewBook = document.querySelector('.new-book');
-  if (addNewBook.classList.contains('vanish') === false) {
-    addNewBook.classList.add('vanish');
-  }
-  const contact = document.querySelector('.contact');
-  if (contact.classList.contains('vanish') === true) {
-    contact.classList.remove('vanish');
-  }
-  const bookDisplay = document.querySelector('.awesome-books');
-  if (bookDisplay.classList.contains('vanish') === false) {
-    bookDisplay.classList.add('vanish');
-  }
-};
-
-const addBookkkkk = () => {
-  const addNewBook = document.querySelector('.new-book');
-  if (addNewBook.classList.contains('vanish') === true) {
-    addNewBook.classList.remove('vanish');
-  }
-  const contact = document.querySelector('.contact');
-  if (contact.classList.contains('vanish') === false) {
-    contact.classList.add('vanish');
-  }
-  const bookDisplay = document.querySelector('.awesome-books');
-  if (bookDisplay.classList.contains('vanish') === false) {
-    bookDisplay.classList.add('vanish');
-  }
-};
-
-const toHome = document.querySelector('.home');
-toHome.addEventListener('click', () => {
-  homePage();
-});
-
-const Home = document.querySelector('.Home');
-Home.addEventListener('click', () => {
-  homePage();
-});
-
-const toAddNew = document.querySelector('.add-new');
-toAddNew.addEventListener('click', () => {
-  addBookkkkk();
-});
-
-const toContact = document.querySelector('.contact-link');
-toContact.addEventListener('click', () => {
-  contactArea();
-});
-
-const tolist = document.querySelector('.list');
-tolist.addEventListener('click', () => {
-  homePage();
-});
+Navigation();
